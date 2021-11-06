12c1b5e2cfdd4f4bbbb19c67e1609c3152eeac46
Koen Haverkort <k.j.haverkort@students.uu.nl>
2021-11-06 10:22:48 +0100

    Add contents of the ontology, tool annotations, run config, and constraints file modals

--- front-end/src/components/Domain/Domain.tsx
+++ front-end/src/components/Domain/Domain.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable import/prefer-default-export */
 /*
  * This program has been developed by students from the bachelor Computer Science at
@@ -7,13 +8,15 @@
  */
 
 /**
- * Shared behaviour for the Domain components.
+ * Shared behavior for the Domain components.
  *
  * @packageDocumentation
  */
 import React, { ReactNode } from 'react';
-import { Modal } from 'antd';
+import { Modal, Typography } from 'antd';
 import { Topic } from '@models/Domain';
+
+const { Paragraph } = Typography;
 
 /**
  * Fetch all topics from the back-end.
@@ -69,8 +72,14 @@ export function tooltipModal(
  * @returns The tooltip modal for ontology files.
  */
 export function ontologyModal(visible: boolean, cancel: () => void) {
-  return tooltipModal('Ontology file', visible, cancel, (
-    <p>Information about ontology files.</p>
+  return tooltipModal('How to create an ontology file', visible, cancel, (
+    <div>
+      <Paragraph>Ontology file should specify the<strong>&nbsp;ontology (taxonomy)&nbsp;</strong>that&nbsp;specifies the domain vocabulary. It should be provided in the<strong> OWL </strong>or<strong> RDF format.</strong>&nbsp;</Paragraph>
+      <Paragraph><em><a href="https://protege.stanford.edu/" target="_blank" rel="noreferrer noopener">Prot&eacute;g&eacute;</a>&nbsp;is the suggested ontology editor.</em></Paragraph>
+      <Paragraph>An exemplary ontology file can be downloaded&nbsp;<a href="https://raw.githubusercontent.com/sanctuuary/APE_UseCases/master/ImageMagick/imagemagick_taxonomy.owl" target="_blank" rel="noreferrer noopener">here</a>&nbsp;(the exemplary domain is defined over the&nbsp;<a href="https://ape-framework.readthedocs.io/en/latest/docs/demo/imagemagick/imagemagick.html" target="_blank" rel="noreferrer noopener">Image Magick</a> toolset).&nbsp;</Paragraph>
+      <Paragraph>&nbsp;&nbsp;</Paragraph>
+      <Paragraph><strong>For a detailed description of the ontology file we refer to the corresponding&nbsp;<a href="https://ape-framework.readthedocs.io/en/latest/docs/specifications/setup.html#id1" target="_blank" rel="noreferrer noopener">readthedocs</a> documentation.&nbsp;</strong></Paragraph>
+    </div>
   ));
 }
 
@@ -81,8 +90,13 @@ export function ontologyModal(visible: boolean, cancel: () => void) {
  * @returns The tooltip modal for tool annotations files.
  */
 export function toolAnnotationsModal(visible: boolean, cancel: () => void) {
-  return tooltipModal('Tool annotations file', visible, cancel, (
-    <p>Information about tool annotations.</p>
+  return tooltipModal('How to create a tool annotations file', visible, cancel, (
+    <div>
+      <Paragraph>Tool annotation file should specify the<strong>&nbsp;tool annotations </strong>with respect to their&nbsp;<strong>inputs&nbsp;</strong>and&nbsp;<strong>outputs</strong>.&nbsp;It should be provided in the<strong> JSON</strong><strong>&nbsp;format.</strong>&nbsp;</Paragraph>
+      <Paragraph>An exemplary tool annotation file can be downloaded&nbsp;<a href="https://raw.githubusercontent.com/sanctuuary/APE_UseCases/master/ImageMagick/tool_annotations.json" target="_blank" rel="noreferrer noopener">here</a>&nbsp;(the exemplary domain is defined over the&nbsp;<a href="https://ape-framework.readthedocs.io/en/latest/docs/demo/imagemagick/imagemagick.html" target="_blank" rel="noreferrer noopener">Image Magick</a> toolset).&nbsp;</Paragraph>
+      <Paragraph>&nbsp;&nbsp;</Paragraph>
+      <Paragraph><strong>For a detailed description of the tool annotation file we refer to the corresponding&nbsp;<a href="https://ape-framework.readthedocs.io/en/latest/docs/specifications/setup.html#tool-annotations" target="_blank" rel="noreferrer noopener">readthedocs</a> documentation.&nbsp;</strong></Paragraph>
+    </div>
   ));
 }
 
@@ -93,8 +107,13 @@ export function toolAnnotationsModal(visible: boolean, cancel: () => void) {
  * @returns The tooltip modal for run config files.
  */
 export function runConfigModal(visible: boolean, cancel: () => void) {
-  return tooltipModal('Run configuration file', visible, cancel, (
-    <p>Information about the run configuration.</p>
+  return tooltipModal('How to create a run configuration file', visible, cancel, (
+    <div>
+      <Paragraph>Run configuration file should specify the<strong> basic run configuration</strong> (timeout, expected number of solution, etc.) as well as the <strong>inputs </strong>and<strong> outputs</strong> for the demonstration synthesis run.&nbsp;</Paragraph>
+      <Paragraph>An exemplary configuration file can be downloaded&nbsp;<a href="https://raw.githubusercontent.com/sanctuuary/APE_UseCases/master/ImageMagick/Example1/config.json" target="_blank" rel="noreferrer noopener">here</a>&nbsp;(the exemplary domain is defined over the&nbsp;<a href="https://ape-framework.readthedocs.io/en/latest/docs/demo/imagemagick/imagemagick.html" target="_blank" rel="noreferrer noopener">Image Magick</a> toolset).</Paragraph>
+      <Paragraph>&nbsp;</Paragraph>
+      <Paragraph><strong>For a detailed description of the configuration file we refer to&nbsp;the corresponding <a href="https://ape-framework.readthedocs.io/en/latest/docs/specifications/setup.html#configuration-file" target="_blank" rel="noreferrer noopener">readthedocs</a> documentation.&nbsp;</strong></Paragraph>
+    </div>
   ));
 }
 
@@ -105,7 +124,12 @@ export function runConfigModal(visible: boolean, cancel: () => void) {
  * @returns The tooltip modal for constraints files.
  */
 export function constraintsModal(visible: boolean, cancel: () => void) {
-  return tooltipModal('Constraints file', visible, cancel, (
-    <p>Information about the constraints file.</p>
+  return tooltipModal('How to create a constraints file', visible, cancel, (
+    <div>
+      <Paragraph>Constraints file should specify the<strong>&nbsp;constraints </strong>used<strong>&nbsp;</strong>for the demonstration synthesis run.&nbsp;</Paragraph>
+      <Paragraph>An exemplary configuration file can be downloaded&nbsp;<a href="https://raw.githubusercontent.com/sanctuuary/APE_UseCases/master/ImageMagick/Example1/constraints.json" target="_blank" rel="noreferrer noopener">here</a>&nbsp;(the exemplary domain is defined over the&nbsp;<a href="https://ape-framework.readthedocs.io/en/latest/docs/demo/imagemagick/imagemagick.html" target="_blank" rel="noreferrer noopener">Image Magick</a> toolset).</Paragraph>
+      <Paragraph>&nbsp;</Paragraph>
+      <Paragraph><strong>For a detailed description of the constraints file we refer to&nbsp;the corresponding <a href="https://ape-framework.readthedocs.io/en/latest/docs/specifications/setup.html#constraints-file" target="_blank" rel="noreferrer noopener">readthedocs</a> documentation.&nbsp;</strong></Paragraph>
+    </div>
   ));
 }
